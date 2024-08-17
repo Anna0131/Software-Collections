4bc5b43d304c75e5351ebb54824fe8824740c205
tommygood <tommy50508@gmail.com>
2024-08-17 15:13:30 +0800

    add loading animation at login

--- js/login.js
+++ js/login.js
@@ -1,8 +1,20 @@
+function showOrHideLoading() {
+	const spinner = document.getElementsByClassName("spinner")[0];
+	if (spinner.style.display === 'none' || spinner.style.display === '') {
+    	spinner.style.display = 'block'; // 显示加载动画
+    }
+	else {
+        spinner.style.display = 'none';  // 隐藏加载动画
+    }
+}
+
 async function submit() {
+	showOrHideLoading();
     const account = document.getElementById("account").value;
     const password = document.getElementById("password").value;
     data = {account : account, password : password};
     let suc_login = await axios.post('/api/login', data);
+	showOrHideLoading();
     suc_login = suc_login.data;
     if (suc_login.suc) {
         location.href = '/main';
